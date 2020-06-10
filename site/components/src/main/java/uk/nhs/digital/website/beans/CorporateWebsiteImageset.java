8bad8d9d8eb0d79470efc5489e6f102d1448d005
Jodi Warren <jodi@jodiwarren.com>
2020-06-10 23:07:14 +0100

    [DW-1495] Create 2x image sizes

--- site/components/src/main/java/uk/nhs/digital/website/beans/CorporateWebsiteImageset.java
+++ site/components/src/main/java/uk/nhs/digital/website/beans/CorporateWebsiteImageset.java
@@ -9,6 +9,11 @@ import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerat
 @Node(jcrType = "website:corporateWebsiteImageset")
 public class CorporateWebsiteImageset extends HippoGalleryImageSet {
 
+    @HippoEssentialsGenerated(internalName = "website:homeBlogThumb2x")
+    public HippoGalleryImageBean getHomeBlogThumb2x() {
+        return getBean("website:homeBlogThumb2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:homeBlogThumb")
     public HippoGalleryImageBean getHomeBlogThumb() {
         return getBean("website:homeBlogThumb", HippoGalleryImageBean.class);
@@ -19,9 +24,19 @@ public class CorporateWebsiteImageset extends HippoGalleryImageSet {
         return getBean("website:homeBlogThumbSmall", HippoGalleryImageBean.class);
     }
 
+    @HippoEssentialsGenerated(internalName = "website:homeBlogThumbSmall2x")
+    public HippoGalleryImageBean getHomeBlogThumbSmall2x() {
+        return getBean("website:homeBlogThumbSmall2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:newsFeaturedPost")
     public HippoGalleryImageBean getNewsFeaturedPost() {
         return getBean("website:newsFeaturedPost", HippoGalleryImageBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:newsFeaturedPost2x")
+    public HippoGalleryImageBean getNewsFeaturedPost2x() {
+        return getBean("website:newsFeaturedPost2x", HippoGalleryImageBean.class);
     }
 
     @HippoEssentialsGenerated(internalName = "website:newsPostImage")
@@ -29,9 +44,19 @@ public class CorporateWebsiteImageset extends HippoGalleryImageSet {
         return getBean("website:newsPostImage", HippoGalleryImageBean.class);
     }
 
+    @HippoEssentialsGenerated(internalName = "website:newsPostImage2x")
+    public HippoGalleryImageBean getNewsPostImage2x() {
+        return getBean("website:newsPostImage2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:newsThumbnail")
     public HippoGalleryImageBean getNewsThumbnail() {
         return getBean("website:newsThumbnail", HippoGalleryImageBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:newsThumbnail2x")
+    public HippoGalleryImageBean getNewsThumbnail2x() {
+        return getBean("website:newsThumbnail2x", HippoGalleryImageBean.class);
     }
 
     @HippoEssentialsGenerated(internalName = "website:oldArticleThumbnail")
@@ -39,9 +64,19 @@ public class CorporateWebsiteImageset extends HippoGalleryImageSet {
         return getBean("website:oldArticleThumbnail", HippoGalleryImageBean.class);
     }
 
+    @HippoEssentialsGenerated(internalName = "website:oldArticleThumbnail2x")
+    public HippoGalleryImageBean getOldArticleThumbnail2x() {
+        return getBean("website:oldArticleThumbnail2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:pageHeaderHeroModule")
     public HippoGalleryImageBean getPageHeaderHeroModule() {
         return getBean("website:pageHeaderHeroModule", HippoGalleryImageBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:pageHeaderHeroModule2x")
+    public HippoGalleryImageBean getPageHeaderHeroModule2x() {
+        return getBean("website:pageHeaderHeroModule2x", HippoGalleryImageBean.class);
     }
 
     @HippoEssentialsGenerated(internalName = "website:pageHeaderHeroModuleSmall")
@@ -49,9 +84,19 @@ public class CorporateWebsiteImageset extends HippoGalleryImageSet {
         return getBean("website:pageHeaderHeroModuleSmall", HippoGalleryImageBean.class);
     }
 
+    @HippoEssentialsGenerated(internalName = "website:pageHeaderHeroModuleSmall2x")
+    public HippoGalleryImageBean getPageHeaderHeroModuleSmall2x() {
+        return getBean("website:pageHeaderHeroModuleSmall2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:pageHeaderSlimBanner")
     public HippoGalleryImageBean getPageHeaderSlimBanner() {
         return getBean("website:pageHeaderSlimBanner", HippoGalleryImageBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:pageHeaderSlimBanner2x")
+    public HippoGalleryImageBean getPageHeaderSlimBanner2x() {
+        return getBean("website:pageHeaderSlimBanner2x", HippoGalleryImageBean.class);
     }
 
     @HippoEssentialsGenerated(internalName = "website:pageHeaderSlimBannerSmall")
@@ -59,9 +104,19 @@ public class CorporateWebsiteImageset extends HippoGalleryImageSet {
         return getBean("website:pageHeaderSlimBannerSmall", HippoGalleryImageBean.class);
     }
 
+    @HippoEssentialsGenerated(internalName = "website:pageHeaderSlimBannerSmall2x")
+    public HippoGalleryImageBean getPageHeaderSlimBannerSmall2x() {
+        return getBean("website:pageHeaderSlimBannerSmall2x", HippoGalleryImageBean.class);
+    }
+
     @HippoEssentialsGenerated(internalName = "website:postImageSmall")
     public HippoGalleryImageBean getPostImageSmall() {
         return getBean("website:postImageSmall", HippoGalleryImageBean.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:postImageSmall2x")
+    public HippoGalleryImageBean getPostImageSmall2x() {
+        return getBean("website:postImageSmall2x", HippoGalleryImageBean.class);
     }
 
 }
